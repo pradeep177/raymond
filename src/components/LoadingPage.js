4f0b815e73d6e1204d62748d488e5449744f380f
pradeep177 <72063995+pradeep177@users.noreply.github.com>
2021-03-04 12:30:58 +0530

    Update LoadingPage.js

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -10,18 +10,6 @@ function LoadingPage() {
         <div className="loading-page">  
             <img src={picture} width="250px"height="350px" alt="laoding-img"></img>
             <h3 className="text-red">The First Of A Kind Virtual Trade Show </h3>
-            <div className="first-hover">
-                <span className="span1">Note</span>
-                <div className="display-hover1">
-                    lorem ipsum of 5lorem ipsum of 5lorem ipsum of 5lorem ipsum of 5
-                </div>
-            </div>
-            <div className="second-hover">
-                <span className="span2">Note</span>
-                <div className="display-hover2">
-                    lorem ipsum of 5lorem ipsum of 5lorem ipsum of 5lorem ipsum of 5
-                </div>
-            </div>
             <p className="text-red">@ RAYMONDS 2021 ALL RIGHTS RESERVED</p>
         </div>
         </>
